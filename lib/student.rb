7795520f9818de00037a0e4c1149e9d928a6535d
jessicatriana <jessica.triana.tx@gmail.com>
2020-02-04 13:20:57 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(a_string) 
+        @knowledge << a_string
+    end
+
+    def knowledge 
+        @knowledge
+    end
 
 end
