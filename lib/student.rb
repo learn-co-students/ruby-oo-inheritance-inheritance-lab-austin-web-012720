6dec04720dfc43cc4dc013bd627fb2b03f33c627
mattjagiello <mjagiello@gmail.com>
2020-02-04 12:52:14 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,19 @@
-class Student 
+class Student < User
+
+    def initialize
+        @knowledge = []
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+    def knowledge=()
+        @knowledge
+    end
+
+    def learn(know_string)
+        @knowledge << know_string
+    end
 
 end
